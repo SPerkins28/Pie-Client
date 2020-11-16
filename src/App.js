c49d35dc138d35e825496fd4d6e1e4f826673925
Sean Perkins <seanperkins@Seans-MBP.hsd1.in.comcast.net>
2020-11-16 11:42:24 -0500

    added login signup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {useState} from 'react';
 import Navbar from './Components/Navbar/Navbar';
+import Auth from './Components/Auth/Auth'
 import './App.css';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <p>Hey this is pretty cool!</p>
       <p>{name}</p> */}
       <Navbar />
+      <Auth />
     </div>
   );
 }
---
+++ src/Components/Auth/Auth.jsx
@@ -0,0 +1,68 @@
+import {useState} from 'react';
+import './Auth.css';
+
+const Auth = () => {
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [login, setLogin] = useState(true);
+
+    const title = () => {
+        return login ? 'Login' : 'Signup'
+        //@ If login is true return string of 'login'. Else, return 'Signup'; 
+    }
+
+    const loginToggle = (event) => {
+        event.preventDefault();
+        setLogin(!login) //@ => set Login to be the opposite of its current value
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+        setPassword("");
+    }
+
+    const signUpFields = () => {
+        return !login ? (
+            <div>
+                <label htmlFor="firstName">First Name</label>
+                <br/>
+                <input type="text" id="firstName" value={firstName} onChange={(event) => setFirstName(event.target.value)} />
+                <br/>
+                <label htmlFor="lastName">Last Name</label>
+                <br/>
+                <input type="text" id="lastName" value={lastName} onChange={(event) => setLastName(event.target.value)} />
+            </div>
+        ) : null //@ if login comes back as false, display all of these things else, display none
+    }
+
+    return(
+        <div>
+            <form>
+                <h1>{title()}</h1>
+                {signUpFields()}
+                <label htmlfor="email">Email:</label>
+                <br/>
+                <input type="text" id="email" placeholder="email@email.com" value={email} onChange={(event) => {
+                //@ two-way data binding. This builds a closed circuit to display and update an input field
+                //@ The data comes in and changes the state variable via setEmail. The stat variable (email) is tied to the input field via the value attribute.
+                    console.log(event);
+                    setEmail(event.target.value);
+                }} />
+                <br/>
+                <label htmlfor="password">Password:</label>
+                <br/>
+                <input type="password" id="password" value={password} onChange={(event) => {
+                    console.log(event);
+                    setPassword(event.target.value)
+                }} />
+                <br/>
+                <button onClick={loginToggle}>Login/Signup Toggle</button>
+            </form>
+        </div>
+    );
+};
+
+export default Auth;
+
